5a99582eaba5c7231360f8d517b9eb161b91b6ad
Marilyn Carlin and phat nguyen <marilyn+phat@>
2017-05-23 16:24:21 -0700

    modify Task.cs to remove Save function, and recreate that functionality by using id fields in object descriptions.

--- Objects/Task.cs
+++ Objects/Task.cs
@@ -1,14 +1,17 @@
 using System.Collections.Generic;
 
-namespace ToDoList.Objects
+namespace ToDoList
 {
   public class Task
   {
     private string _description;
-    private static List<string> _instances = new List<string> {};
+    private int _id;
+    private static List<Task> _instances = new List<Task> {};
     public Task (string description)
     {
       _description = description;
+      _instances.Add(this);
+      _id = _instances.Count;
     }
     public string GetDescription()
     {
@@ -18,17 +21,21 @@ namespace ToDoList.Objects
     {
       _description = newDescription;
     }
-    public static List<string> GetAll()
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Task> GetAll()
     {
       return _instances;
-    }
-    public void Save()
-    {
-      _instances.Add(_description);
     }
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+    public int GetId()
+    {
+      return _id;
     }
   }
 }
